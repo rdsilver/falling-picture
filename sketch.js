d7a4a6624dd5c79d4c0ee315abfbab0df60106f5
Reed <reedsilverstein@gmail.com>
2019-09-11 23:00:29 -0500

    Faster, no temp array

--- sketch.js
+++ sketch.js
@@ -1,32 +1,15 @@
-let columnsWithPixels = [];
-let tempPixels;
+let img;
 
 function preload() {
-  img = loadImage("assets/zuckerberg.png");
+  img = loadImage("assets/clinton.png");
 }
 
 function setup() {
   pixelDensity(1);
-  canvas = createCanvas(img.width, img.height);
+  canvas = createCanvas(400, 700);
   canvas.parent('sketch');
-  image(img, 0, 0);
-  findColumnsWithPixels();
-}
-
-function findColumnsWithPixels() {
-  loadPixels();
-  for(let x=0;x<width;x++) {
-    for(let y=height;y>=0;y--) {
-      const index = (x+y*width)*4;
-
-      if (columnsWithPixels.indexOf(x) >= 0)
-        continue;
-
-      if (pixels[index]) {
-        columnsWithPixels.push(x);
-      }
-    }
-  }
+  imageMode(CENTER);
+  angleMode(DEGREES);
 }
 
 function draw() {
@@ -35,33 +18,37 @@ function draw() {
 
 function dropPixels() {
   loadPixels();
-  tempPixels = new Array(pixels.length).fill(-1);
-  for(let i=0;i<columnsWithPixels.length;i++) {
-    for(let y=height; y >= 0; y--) {
-      let x = columnsWithPixels[i];
+  for(let x=0;x<width;x++) {
+    for(let y=height-1;y>=0;y--) {
       let index = (x+y*width)*4;
-      let oneAbove = (x+(y+1)*width)*4;
+      let indexBelow = (x+(y+1)*width)*4;
       if (y+1 < height ) {
-        if (!pixels[oneAbove]) {
-          let color = [pixels[index], pixels[index+1], pixels[index+2] ,pixels[index+3]]
-          setPixel(oneAbove, color);
-          setPixel(index, [null, null, null, null]);
+        if (!pixels[indexBelow+3]) {
+          let indexColor = [pixels[index], pixels[index+1], pixels[index+2] ,pixels[index+3]]
+          let indexBelowColor = [pixels[indexBelow], pixels[indexBelow+1], pixels[indexBelow+2] ,pixels[indexBelow+3]]
+          setPixel(indexBelow, indexColor);
+          setPixel(index, indexBelowColor);
         }
       }
-    }
-  }
-
-  for(let i=0; i<tempPixels.length; i++) {
-    if (tempPixels[i] != -1) {
-      pixels[i] = tempPixels[i];
     }
   }
   updatePixels();
 }
 
 function setPixel(index, color) {
-  tempPixels[index] = color[0];
-  tempPixels[index+1] = color[1];
-  tempPixels[index+2] = color[2];
-  tempPixels[index+3] = color[3];
+  pixels[index] = color[0];
+  pixels[index+1] = color[1];
+  pixels[index+2] = color[2];
+  pixels[index+3] = color[3];
+}
+
+
+function mouseClicked() {
+  $('.frame').removeClass('noClick');
+  push()
+  translate(mouseX, mouseY);
+  rotate(Math.random() * 5000);
+  scale(.75);
+  image(img, 0, 0);
+  pop();
 }
--- sketchwithgifstuff.js
+++ sketchwithgifstuff.js
@@ -1,61 +1,111 @@
-var img;
+var img, img2;
 var gif;
 var fileName = 'melted.webm';
-var saved = true;
+var saved = false;
+let columnsWithPixels = [];
+let tempPixels;
 
 function preload() {
-  img = loadImage("assets/monaLisa.jpg");
+  img1 = loadImage("assets/clinton.png");
+  img2 = loadImage("assets/zuckerberg.png");
 }
 
 function setup() {
   pixelDensity(1);
-  canvas = createCanvas(img.width, img.height);
+  canvas = createCanvas(400, 700);
   canvas.parent('sketch');
-  image(img, 0, 0);
+  imageMode(CENTER);
+  angleMode(DEGREES);
+  // push()
+  // translate (width/2, height/2);
+  // rotate(noise(frameCount) * 100);
+  // image(img, 0, 0);
+  // pop();
+  //findColumnsWithPixels();
 
-  gif = new CCapture({
-    framerate: 24,
-    quality: 90,
-    format: 'webm',
-    name: fileName,
-    workersPath: 'gifLibrary/'
-  });
+  // var bigint = checkColors();
+  // gif = new CCapture({
+  //   framerate: 60,
+  //   quality: 50,
+  //   format: 'webm',
+  //   name: fileName,
+  //   //transparent: bigint,
+  //   //transparent: null,
+  //   workersPath: 'gifLibrary/'
+  // });
 
-  gif.start();
+  // gif.start();
 }
+
+// function findColumnsWithPixels() {
+//   loadPixels();
+//   for(let x=0;x<width;x++) {
+//     for(let y=height-1;y>=0;y--) {
+//       const index = (x+y*width)*4;
+//
+//       if (columnsWithPixels.indexOf(x) >= 0)
+//         continue;
+//
+//       if (pixels[index]) {
+//         columnsWithPixels.push(x);
+//       }
+//     }
+//   }
+// }
 
 function draw() {
-  if (frameCount<img.height && frameCount%5 == 0)
-    gif.capture(canvas.elt);
-  else if (frameCount == img.height && !saved) {
-    gif.save(doneRendering);
-    saved = true;
-  }
+  // if (frameCount<2000 && frameCount%5 == 0) {
+  //   gif.capture(canvas.elt);
+  // } else if (frameCount == 2000) {
+  //   gif.save(doneRendering);
+  //   saved = true;
+  // }
 
-  sortPixels();
+  // if (frameCount % img.height === 0) {
+  //   translate (Math.random()*width, img.height/2);
+  //   rotate(frameCount*100*Math.random());
+  //   image(img, 0, 0);
+  // }
+
+  dropPixels();
 }
 
-function sortPixels() {
+function dropPixels() {
   loadPixels();
-  for(var x=0;x<width;x++) {
-    for(var y=height;y>=0;y--) {
-      var index = (x+y*width)*4;
-      totalColor = pixels[index] + pixels[index+1] + pixels[index+2];
-      var indexDown = (x+(y+1)*width)*4;
-      totalColorDown = pixels[indexDown] + pixels[indexDown+1] + pixels[indexDown+2];
-
-      if (totalColor < totalColorDown) {
-        tempPixels = [pixels[index], pixels[index+1],pixels[index+2]];
-        pixels[index] = pixels[indexDown];
-        pixels[index+1] = pixels[indexDown+1];
-        pixels[index+2] = pixels[indexDown+2];
-        pixels[indexDown] = tempPixels[0];
-        pixels[indexDown+1] = tempPixels[1];
-        pixels[indexDown+2] = tempPixels[2];
+  for(let x=0;x<width;x++) {
+    for(let y=height-1;y>=0;y--) {
+      let index = (x+y*width)*4;
+      let indexBelow = (x+(y+1)*width)*4;
+      if (y+1 < height ) {
+        if (!pixels[indexBelow+3]) {
+          let indexColor = [pixels[index], pixels[index+1], pixels[index+2] ,pixels[index+3]]
+          let indexBelowColor = [pixels[indexBelow], pixels[indexBelow+1], pixels[indexBelow+2] ,pixels[indexBelow+3]]
+          setPixel(indexBelow, indexColor);
+          setPixel(index, indexBelowColor);
+        }
       }
     }
   }
+
   updatePixels();
+}
+
+function setPixel(index, color) {
+  pixels[index] = color[0];
+  pixels[index+1] = color[1];
+  pixels[index+2] = color[2];
+  pixels[index+3] = color[3];
+}
+
+
+function mouseClicked() {
+  var randomPics = [img1, img2];
+  push()
+  translate(mouseX, mouseY);
+  rotate(Math.random() * 5000);
+  scale(.75);
+  image(img1, 0, 0);
+  pop();
 }
 
 function doneRendering(blob) {
@@ -76,3 +126,30 @@ function saveGif(blob) {
 
   window.URL.revokeObjectURL(url);
 };
+
+/* Finds a color NOT present on canvas
+ * For picking a background for transparency */
+function checkColors() {
+  let colors = {}
+
+  _canvas = document.createElement('canvas')
+  _canvas.width = canvas.width
+  _canvas.height = canvas.height
+
+  let ctx = _canvas.getContext('2d')
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  ctx.drawImage(canvas.elt, 0, 0)
+  let imgdata = ctx.getImageData(0,0,canvas.width,canvas.height).data;
+
+  // Convert "imgdata" to rgb int
+  for (let i = 0, n = imgdata.length; i < n; i += 4) {
+    let integer = ( (imgdata[i] << 16) + (imgdata[i+1] << 8) + imgdata[i+2])
+    colors[integer] = true;
+  }
+
+  for (let i = 0; i < 0xFFFFFF; i += 0x000001) {
+    if (!colors[i]) {
+      return i;
+    }
+  }
+}
